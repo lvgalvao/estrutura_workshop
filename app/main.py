4516d597ab9c1b6591a487bfbd50d02cee742cc1
Luciano Filho <lvgalvaofilho@gmail.com>
2023-10-07 12:52:38 -0300

    print

--- app/main.py
+++ app/main.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
     data_frame_list = extract_from_excel("data/input")
     data_frame = contact_data_frames(data_frame_list)
     load_excel(data_frame, "data/output", "output")
+    print("Done!")
